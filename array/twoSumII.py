a4a89e7d007d0ea81baada496fe60d36b1207ecd
saomahendra <mahendra_sao@yahoo.com>
2024-05-15 22:22:43 -0400

    Array solutions

--- array/twoSumII.py
+++ array/twoSumII.py
@@ -11,5 +11,5 @@ class Solution:
 				return [l+1, r +1]
 		return []
 print(Solution().findTwoSum([1,3,4,5,7,10,11], 9))
-#source https://www.youtube.com/watch?v=cQ1Oz4ckceM&ab_channel=NeetCode
+
 
