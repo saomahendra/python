68b7155fe3c317dd0abfc0090b006eb996fe43da
saomahendra <mahendra_sao@yahoo.com>
2024-05-16 15:51:32 -0400

    Count bits in integers

---
+++ bit-manipulation/counting-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+	def countBits(self, n):
+		offset = 1
+		dp = [0] * (n+1)
+		for i in range(1, n + 1):
+			if offset * 2 == i:
+				offset = i
+			dp[i] = 1 + dp[i - offset]
+		return dp
+print(Solution().countBits(5))
